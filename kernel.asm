2b814e964fb28ac2451905eb77e6687ab4452c67
lol <gge>
2022-05-28 10:54:33 +0200

    wierd boot loop,half solution

--- kernel.asm
+++ kernel.asm
@@ -1,9 +1,35 @@
-mov ah,09h
-mov al,'h'
-mov bh,0
-mov bl,4
-mov cx,1
 
-int 10h
+jmp protect
+
+%include "gdt.asm"
+
+protect:
+call enableA20
+cli
+lgdt [gdtdesc]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp codeseg:startPM
+
+enableA20:
+in al,0x92
+or al,2
+out 0x92,al
+ret
+
+[bits 32]
+startPM:
+
+mov ax,dataseg
+mov ds,ax
+mov es,ax
+mov fs,ax
+mov gs,ax
+mov ss,ax
+
+mov [0xb80000],byte 'h'
+
+jmp $
 
 times 2048-($-$$)  db 0
