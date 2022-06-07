344bab4ee8b2f1ae5a4f6d9f06b1e1781b841508
lol <gge>
2022-06-07 17:09:07 +0200

    32 bit protected mode but it doesn't work

--- boot/stage2/stage2.asm
+++ boot/stage2/stage2.asm
@@ -3,11 +3,58 @@ extern cstart
 global entry
 
 entry:
-mov ah,09h
-mov al,'N'
-mov bh,0
-mov bl,4
-mov cx,1
-int 10h
-call cstart
+CODE_SEG equ GDT_code - GDT_start
+DATA_SEG equ GDT_data - GDT_start
+
+cli
+lgdt [GDT_descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp CODE_SEG:start_protected_mode
+
+jmp $
+                                    
+BOOT_DISK: db 0
+
+GDT_start:
+    GDT_null:
+        dd 0x0
+        dd 0x0
+
+    GDT_code:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10011010
+        db 0b11001111
+        db 0x0
+
+    GDT_data:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10010010
+        db 0b11001111
+        db 0x0
+
+GDT_end:
+
+GDT_descriptor:
+    dw GDT_end - GDT_start - 1
+    dd GDT_start
+
+
+[bits 32]
+start_protected_mode:
+    mov ax, DATA_SEG
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
+	mov ebp, 0x90000		; 32 bit stack base pointer
+	mov esp, ebp
+    call cstart
 jmp $
