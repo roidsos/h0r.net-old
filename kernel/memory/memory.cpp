4f46d35a82e0c007681546e1a3e8e2b34cbe8fc3
lol <gge>
2022-06-23 11:27:06 +0200

    memcmp function

--- kernel/memory/memory.cpp
+++ kernel/memory/memory.cpp
@@ -51,3 +51,13 @@ void memcpy(void* dest,void* src,uint_64 num){
     }
 
 } 
+int memcmp(const void* aptr,const void* bptr,size_t n){
+	const unsigned char* a = aptr,b = bptr;
+	for (size_t i = 0; i < n; i++)
+	{
+		if(a[i] < b[i]) return -1;
+		else if(a[i] > b[i]) return 1;
+	}
+	return 0;
+	
+}
