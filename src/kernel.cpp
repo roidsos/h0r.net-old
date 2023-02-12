5c05cd53333fa0f10d596fd12f34ce503d479b0e
lol <gge>
2023-02-12 09:54:04 +0100

    .

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -19,14 +19,15 @@ extern "C" int kernel_main()
     Clearscr(LIGHT_BLUE);
     enable_text_cursor(14, 15);
 
-    GDT gdt;
-
-    gdt.AddSegment(0, 0, 0);//null
-    gdt.AddSegment(0x00AF, 0x000FFFFF, 0x9B);//code
-    gdt.AddSegment(0x00AF, 0x000FFFFF, 0x93);//data
-    gdt.AddSegment(0, 0x000FFFFF, (GDT_DATA_PL3));
-
-    gdt.Load();
+//gdts are fucked
+//    GDT gdt;
+//
+//    gdt.AddSegment(0, 0, 0);//null
+//    gdt.AddSegment(0x00AF, 0x000FFFFF, 0x9B);//code
+//    gdt.AddSegment(0x00AF, 0x000FFFFF, 0x93);//data
+//    gdt.AddSegment(0, 0x000FFFFF, (GDT_DATA_PL3));
+//
+//    gdt.Load();
 
     InitDrivers();
 //    uint_8 test[512];
