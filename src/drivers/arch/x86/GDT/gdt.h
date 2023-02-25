e5e5112947b2305910882088b47d10864778272e
lolguy91 <95227041+lolguy91@users.noreply.github.com>
2023-02-25 09:40:12 +0000

    updated the GDT to have all the things our os will need for now

--- src/drivers/arch/x86/GDT/gdt.h
+++ src/drivers/arch/x86/GDT/gdt.h
@@ -27,20 +27,28 @@
 #define SEG_CODE_EXRDCA    0x0F // Execute/Read, conforming, accessed
  
 #define GDT_CODE_PL0 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
-                     SEG_LONG(1)     | SEG_SIZE(0) | SEG_GRAN(0) | \
+                     SEG_LONG(1)     | SEG_SIZE(0) | SEG_GRAN(1) | \
                      SEG_PRIV(0)     | SEG_CODE_EX
  
 #define GDT_DATA_PL0 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
-                     SEG_LONG(1)     | SEG_SIZE(1) | SEG_GRAN(1) | \
+                     SEG_LONG(1)     | SEG_SIZE(0) | SEG_GRAN(1) | \
                      SEG_PRIV(0)     | SEG_DATA_RDWR
  
 #define GDT_CODE_PL3 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
-                     SEG_LONG(1)     | SEG_SIZE(1) | SEG_GRAN(1) | \
-                     SEG_PRIV(3)     | SEG_CODE_EXRD
+                     SEG_LONG(1)     | SEG_SIZE(0) | SEG_GRAN(1) | \
+                     SEG_PRIV(3)     | SEG_CODE_EX
  
 #define GDT_DATA_PL3 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
                      SEG_LONG(1)     | SEG_SIZE(1) | SEG_GRAN(1) | \
                      SEG_PRIV(3)     | SEG_DATA_RDWR
+
+#define GDT16_CODE_PL0  SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
+                        SEG_LONG(0)     | SEG_SIZE(0) | SEG_GRAN(1) | \
+                        SEG_PRIV(0)     | SEG_CODE_EX
+ 
+#define GDT16_DATA_PL0  SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
+                        SEG_LONG(0)     | SEG_SIZE(0) | SEG_GRAN(1) | \
+                        SEG_PRIV(0)     | SEG_DATA_RDWR
                     
 class GDT{
     uint_8 fullness;
--- src/kernel.cpp
+++ src/kernel.cpp
@@ -19,12 +19,16 @@ extern "C" int kernel_main()
     Clearscr(LIGHT_BLUE);
     enable_text_cursor(14, 15);
 
-     GDT gdt;
-     gdt.AddSegment(0, 0, 0);//null
-     gdt.AddSegment(0x00, 0x000FFFFF, GDT_CODE_PL0);//code
-     gdt.AddSegment(0x00F, 0x000FFFFF, 0x93);//data
+    GDT gdt;
+    gdt.AddSegment(0, 0, 0);//null
+    gdt.AddSegment(0x00, 0x000FFFFF, GDT_CODE_PL0);//64 bit pl 0 code segment
+    gdt.AddSegment(0x00F, 0x000FFFFF, GDT_DATA_PL0);//64 bit pl 0 data segment
+    gdt.AddSegment(0x00, 0x000FFFFF, GDT_CODE_PL3);//64 bit pl 3 code segment
+    gdt.AddSegment(0x00F, 0x000FFFFF, GDT_DATA_PL3);//64 bit pl 3 data segmen
+    gdt.AddSegment(0x00, 0x000FFFFF, GDT16_CODE_PL0);//16 bit pl 3 code segment
+    gdt.AddSegment(0x00F, 0x000FFFFF, GDT16_DATA_PL0);//16 bit pl 3 data segmen
 
-     gdt.Load();
+    gdt.Load();
 
     InitDrivers();
 //    uint_8 test[512];
