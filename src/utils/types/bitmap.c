c5e2149ac655bf2c4691ec9b369d02a4f5a0c883
lolguy91 <retek1544@gmail.com>
2023-08-31 13:10:48 +0200

    one...fucking...bitshift

--- src/utils/types/bitmap.c
+++ src/utils/types/bitmap.c
@@ -21,6 +21,6 @@ bool bitmap_get(struct Bitmap bm,size_t index)
     register size_t byteIndex  = index / 8;
     register uint8_t bitIndex   = index % 8;
 
-    register uint8_t bitIndexer = 1 >> bitIndex;
+    register uint8_t bitIndexer = 1 << bitIndex;
     return bm.buffer[byteIndex] & bitIndexer;
 }
