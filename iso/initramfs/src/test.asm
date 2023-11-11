8533d8fa307d7c1c3a16fb913c9d8f95f67d374b
lolguy91 <retek1544@gmail.com>
2023-11-11 12:00:12 +0100

    printing syscall

--- iso/initramfs/src/test.asm
+++ iso/initramfs/src/test.asm
@@ -1,4 +1,43 @@
 global _start
+
 _start:
+mov rax,1
+mov rbx,'H'
+int 0x80
+mov rax,1
+mov rbx,'e'
+int 0x80
+mov rax,1
+mov rbx,'l'
+int 0x80
+mov rax,1
+mov rbx,'l'
+int 0x80
+mov rax,1
+mov rbx,'o'
+int 0x80
+mov rax,1
+mov rbx,' '
+int 0x80
+mov rax,1
+mov rbx,'W'
+int 0x80
+mov rax,1
+mov rbx,'o'
+int 0x80
+mov rax,1
+mov rbx,'r'
+int 0x80
+mov rax,1
+mov rbx,'l'
+int 0x80
+mov rax,1
+mov rbx,'d'
+int 0x80
+mov rax,1
+mov rbx,'!'
+int 0x80
+mov rax,1
+mov rbx,'\n'
 int 0x80
 jmp $
--- src/core/interface/syscall.c
+++ src/core/interface/syscall.c
@@ -1,7 +1,18 @@
 #include <arch/x86/interrupts/interrupts.h>
 #include <core/logging/logger.h>
+#include <core/kernel.h>
 void syscall_handler(__attribute__((unused)) Registers *regs) {
-    log_info("Syscall LESSGOOOOOOOOO!");
+    switch (regs->rax)
+    {
+    case 0:
+        break;//TODO: exit
+    case 1://printchar
+        flanterm_write(data.ft_ctx,(const char*)&regs->rbx,1);
+        break;
+    
+    default:
+        break;
+    }
     EOI(0x80);
 }
 void init_syscall() { register_ISR(0x80, syscall_handler); }
