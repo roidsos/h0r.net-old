4bd46e994bf7b54c3bf1e0cef975b712a9fd46a2
lolguy91 <retek1544@gmail.com>
2024-05-02 16:00:05 +0000

    IOAPIC fix but no work UwU

--- src/drivers/ACPI/MADT.h
+++ src/drivers/ACPI/MADT.h
@@ -29,40 +29,40 @@ typedef struct {
             uint8_t CPU_ID;
             uint8_t APIC_ID;
             uint32_t flags;
-        } lapic;
+        } __attribute__((__packed__)) lapic;
         struct {
             uint8_t id;
             uint8_t reserved;
             uint32_t adress;
             uint32_t GS_interrupt_base;
-        } ioapic;
+        } __attribute__((__packed__)) ioapic;
         struct {
             uint8_t bus_source;
             uint8_t IRQ_source;
             uint8_t GS_interupt;
             uint16_t flags;
-        } ioapic_override;
+        } __attribute__((__packed__)) ioapic_override;
         struct {
             uint8_t source;
             uint8_t reserved;
             uint16_t flags;
             uint32_t GS_interrupt;
-        } ioapic_nmi;
+        } __attribute__((__packed__)) ioapic_nmi;
         struct {
             uint8_t CPU_ID;
             uint16_t flags;
             uint8_t lint_num;//?????
-        } lapic_nmi;
+        } __attribute__((__packed__)) lapic_nmi;
         struct {
             uint16_t reserved;
             uint64_t addr64;
-        } lapic_addr_overrride;
+        } __attribute__((__packed__)) lapic_addr_overrride;
         struct {
             uint16_t reserved;
             uint32_t ID;
             uint32_t flags;
             uint32_t ACPI_ID;
-        } lx2apic;
+        } __attribute__((__packed__)) lx2apic;
     } the_meat;
 } __attribute__((__packed__)) madt_entry;
 
--- src/drivers/IOAPIC.c
+++ src/drivers/IOAPIC.c
@@ -8,16 +8,16 @@ uint8_t vector_count;
 
 uint32_t read(uint32_t reg)
 {
-   uint32_t volatile *ioapic = (uint32_t volatile *)ioapicaddr;
-   ioapic[0] = (reg & 0xff);
-   return ioapic[4];
+    uint32_t volatile *ioapic = (uint32_t volatile *)ioapicaddr;
+    ioapic[0] = (reg & 0xff);
+    return ioapic[4];
 }
  
 void write( uint32_t reg, uint32_t value)
 {
-   uint32_t volatile *ioapic = (uint32_t volatile *)ioapicaddr;
-   ioapic[0] = (reg & 0xff);
-   ioapic[4] = value;
+    uint32_t volatile *ioapic = (uint32_t volatile *)ioapicaddr;
+    ioapic[0] = (reg & 0xff);
+    ioapic[4] = value;
 }
 
 bool init_ioapic(){
@@ -32,21 +32,81 @@ bool init_ioapic(){
         return false;
     }
     vector_count = ((read(IOAPIC_VER) >> 16) & 0xFF);
-
+    printf("vector count: %u",vector_count);
     // mask all and set 
     for (uint32_t i = 0; i <= vector_count; ++i) {
-        write(IOAPIC_REDTBL+2*i, 0x00010000 | (32 + i));
+        write(IOAPIC_REDTBL+2*i, 1 << 16 | (32 + i));
         write(IOAPIC_REDTBL+2*i+1, 0); // redir cpu
     }
 
     return false;
 }
 
+madt_entry* get_gsi(uint32_t gsi){
+    for(size_t i = 0; i < num_madt_entries;i++){
+        if(entry_ptrs[i]->h.type == MADT_ENTRY_TYPE_IOAPIC
+                && entry_ptrs[i]->the_meat.ioapic.GS_interrupt_base <= gsi
+                    && entry_ptrs[i]->the_meat.ioapic.GS_interrupt_base + vector_count > gsi ){
+            return entry_ptrs[i];
+        }
+    }
+    return NULL;
+}
+
+//TODO: dont aassume lapic ID of 0
 void mask(uint32_t id){
-    write(IOAPIC_REDTBL+2*id,0x00010000 | 1 << 11 | (32 + id));
-    write(IOAPIC_REDTBL+2*id+1, 1 << 23); // redir cpu
+    uint32_t vec = 32 + id;
+    for(size_t i = 0; i < num_madt_entries;i++){
+        if(entry_ptrs[i]->h.type == MADT_ENTRY_TYPE_IOAPIC_OVERRIDE
+                && entry_ptrs[i]->the_meat.ioapic_override.IRQ_source == vec){
+            madt_entry* gsi = get_gsi(entry_ptrs[i]->the_meat.ioapic_override.GS_interupt);
+            void* tmp = ioapicaddr;
+            ioapicaddr = (void*)(uint64_t)gsi->the_meat.ioapic.adress;
+
+            uint64_t redir = id;
+
+            if ((entry_ptrs[i]->the_meat.ioapic_override.flags & (1 << 1)) != 0) {
+                redir |= (1 << 13);
+            }
+            if ((entry_ptrs[i]->the_meat.ioapic_override.flags & (1 << 3)) != 0) {
+                redir |= (1 << 15);
+            }
+
+            write((entry_ptrs[i]->the_meat.ioapic_override.GS_interupt - gsi->the_meat.ioapic.GS_interrupt_base) * 2 + 16, 1 << 16 | (uint32_t)redir);
+            write((entry_ptrs[i]->the_meat.ioapic_override.GS_interupt - gsi->the_meat.ioapic.GS_interrupt_base) * 2 + 17, (uint32_t)(redir >> 32));
+            ioapicaddr = tmp;
+            return;
+        }
+    }
+    madt_entry* gsi = get_gsi(id);
+    write((vec - gsi->the_meat.ioapic.GS_interrupt_base) * 2 + 16,1 << 16 | (vec));
+    write((vec - gsi->the_meat.ioapic.GS_interrupt_base) * 2 + 17,0              ); // redir cpu
 }
 void unmask(uint32_t id){
-    write(IOAPIC_REDTBL+2*id,1 << 11 | (32 + id));
-    write(IOAPIC_REDTBL+2*id+1, 1 << 23); // redir cpu
+    uint32_t vec = 32 + id;
+    for(size_t i = 0; i < num_madt_entries;i++){
+        if(entry_ptrs[i]->h.type == MADT_ENTRY_TYPE_IOAPIC_OVERRIDE
+                && entry_ptrs[i]->the_meat.ioapic_override.IRQ_source == vec){
+            madt_entry* gsi = get_gsi(entry_ptrs[i]->the_meat.ioapic_override.GS_interupt);
+            void* tmp = ioapicaddr;
+            ioapicaddr = (void*)(uint64_t)gsi->the_meat.ioapic.adress;
+
+            uint64_t redir = id;
+
+            if ((entry_ptrs[i]->the_meat.ioapic_override.flags & (1 << 1)) != 0) {
+                redir |= (1 << 13);
+            }
+            if ((entry_ptrs[i]->the_meat.ioapic_override.flags & (1 << 3)) != 0) {
+                redir |= (1 << 15);
+            }
+
+            write((entry_ptrs[i]->the_meat.ioapic_override.GS_interupt - gsi->the_meat.ioapic.GS_interrupt_base) * 2 + 16, (uint32_t) redir);
+            write((entry_ptrs[i]->the_meat.ioapic_override.GS_interupt - gsi->the_meat.ioapic.GS_interrupt_base) * 2 + 17, (uint32_t)(redir >> 32));
+            ioapicaddr = tmp;
+            return;
+        }
+    }
+    madt_entry* gsi = get_gsi(id);
+    write((vec - gsi->the_meat.ioapic.GS_interrupt_base) * 2 + 16,(vec));
+    write((vec - gsi->the_meat.ioapic.GS_interrupt_base) * 2 + 17,0); // redir cpu
 }
