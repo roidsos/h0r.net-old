8c90b17cfdf990121d0fa584e62b07cfa6d949f0
lolguy91 <retek1544@gmail.com>
2024-05-05 14:53:07 +0000

    move a lot of stuff to the RoidsOS repo

--- .devcontainer/install.sh
+++
@@ -1,2 +0,0 @@
-sudo apt update
-sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon grub-pc nasm xorriso
--- cfg/startup.nsh
+++
@@ -1,2 +0,0 @@
-fs0:
-EFI/BOOT/BOOTX64.efi
