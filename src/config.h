7ef0b8b2936278790763359c08f693fed20b0e03
lolguy91 <retek1544@gmail.com>
2024-05-08 14:12:44 +0000

    triggering satanic panic v2

--- src/config.h
+++ src/config.h
@@ -23,7 +23,7 @@
 #define EROR_PORT COM1
 
 //Registery
-#define HEAP_MAGIC 0xB16B00B5B116D1CC // "Big Boobs, Big dick"(I think "Microsoftus Neckbearditis" got me...), UNUSED BTW
+#define HEAP_MAGIC 0xB16B00B569420666 // "Big Boobs, 69 420 666", "(I think "Microsoftus Neckbearditis" got me...), UNUSED BTW
 #define KEY_MAGIC 0xB16B00B5 // "Big Boobs"
 
 #endif
