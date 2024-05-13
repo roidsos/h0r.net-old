417570c32ef79afccb61913c7a8f7d161c9a2ed6
lolguy91 <retek1544@gmail.com>
2024-05-13 13:15:22 +0000

    removed the hello.txt test

--- src/core/sys/wakeup/gaia.c
+++ src/core/sys/wakeup/gaia.c
@@ -8,16 +8,6 @@
 void gaia_main(void){
     printf("TODO: startup tasks\n");
 
-    uint32_t fd = siv_open(0,"hello.txt",SIV_INTENTS_READ);
-    if(fd == UINT32_MAX){
-        printf("Failed to open hello.txt\n");
-        while(true);
-    }
-    char* buf = (char*)malloc(20);
-    siv_read(fd,0,buf,20);
-    printf("hello.txt: %s\n",buf);
-    siv_close(fd);
-
     hive_header* hive = read_hive("hive.reg");
     if(hive == NULL){
         printf("Failed to read hive.reg\n");
