3b4115c66c8ac7ba7416f32e70b745debadbda07
lolguy91 <95227041+lolguy91@users.noreply.github.com>
2024-05-18 13:28:22 +0000

    bruh I left some ded code in

--- src/core/sys/sched/process.c
+++ src/core/sys/sched/process.c
@@ -1,9 +1,1 @@
-void sched_block(uint32_t pid)
-{
-    processes[pid].state_flags = SCHED_STATE_BLOCKED;
-}
-
-void sched_unblock(uint32_t pid)
-{
-    processes[pid].state_flags = SCHED_STATE_READY;
-}
+// Nozhing yet
