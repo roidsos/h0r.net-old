4cdff4ae2b3d81c5c9d79b2082f21c034e91da14
lolguy91 <retek1544@gmail.com>
2024-05-20 16:40:28 +0000

    SHUTDOWN WORKS!

--- src/core/sys/wakeup/gaia.c
+++ src/core/sys/wakeup/gaia.c
@@ -3,11 +3,12 @@
 #include <libk/stddef.h>
 #include <libk/stdint.h>
 #include <vendor/printf.h>
-
+#include <vendor/lai/include/lai/helpers/pm.h>
 #include <utils/log.h>
 
 void gaia_main(void) {
     printf("TODO: startup tasks\n");
+    lai_enter_sleep(5);
     while (true)
         ;
 }
