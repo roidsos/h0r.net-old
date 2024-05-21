8cf93413e2821c87d91d711b74b44032dfd7746f
lolguy91 <retek1544@gmail.com>
2024-05-21 16:10:48 +0000

    removed that stuff that may not work

--- src/core/sys/wakeup/gaia.c
+++ src/core/sys/wakeup/gaia.c
@@ -1,17 +1,9 @@
-#include "core/sys/krnlexec.h"
 #include <core/mm/heap.h>
 #include <libk/stdbool.h>
 #include <vendor/printf.h>
 #include <core/sys/krnlexec.h>
-void syscall_test() {
-    //WARN: Arch specific code in core/ + TODO: smh move to arch/ or make architecture neutral
-    __asm__ volatile ("mov $0, %rax");
-    __asm__ volatile ("int $0x80");
-    printf("If this prints, sys_exit has failed\n"); // wont get here
-}
 
 void gaia_main(void) {
-    printf("TODO: startup tasks\n");
-    execute("syscall_test", syscall_test, sizeof(syscall_test),false);
+    //printf("TODO: startup tasks\n");
     while (true);
 }
