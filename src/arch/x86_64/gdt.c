d643712154309b154c5291d3131b144e50dc10b4
lolguy91 <retek1544@gmail.com>
2024-05-25 12:21:37 +0000

    GDT "fix" but no usermode

--- src/arch/x86_64/gdt.c
+++ src/arch/x86_64/gdt.c
@@ -3,49 +3,51 @@
 
 #include "gdt.h"
 
-gdt_entry gdt[0xFF];
+gdt_table gdt;
 gdt_pointer gdtr;
-tss_entry_t tss_entry;
+tss daTSS;
 
 extern void load_gdt(gdt_pointer *);
 extern void load_tss();
 
 void set_gdt_entry(int i, u16 limit, u16 base, u8 access, u8 flags) {
-    gdt[i].base0 = base & 0xFFFF;
-    gdt[i].base1 = (base >> 16) & 0xFF;
-    gdt[i].base2 = (base >> 24) & 0xFF;
-    gdt[i].limit0 = limit & 0xFFFF;
-    gdt[i].access = access;
-    gdt[i].limit1_flags = ((limit >> 16) & 0x0F) | (flags & 0xF0);
+    gdt.gdt_entries[i].base0 = base & 0xFFFF;
+    gdt.gdt_entries[i].base1 = (base >> 16) & 0xFF;
+    gdt.gdt_entries[i].base2 = (base >> 24) & 0xFF;
+    gdt.gdt_entries[i].limit0 = limit & 0xFFFF;
+    gdt.gdt_entries[i].access = access;
+    gdt.gdt_entries[i].limit1_flags = ((limit >> 16) & 0x0F) | (flags & 0xF0);
 }
-void set_tss_gate(int i, u64 base, u32 limit) {
-    gdt[i].base0 = base & 0xFFFF;
-    gdt[i].base1 = (base >> 16) & 0xFF;
-    gdt[i].base2 = (base >> 24) & 0xFF;
-    gdt[i].limit0 = limit & 0xFFFF;
-    gdt[i].access = 0xE9;
-    gdt[i].limit1_flags = ((limit >> 16) & 0x0F) | 0x00;
+void set_tss_gate(u64 tss) {
+    gdt.tss_entry.length = sizeof(tss);
+    gdt.tss_entry.base = (u16)(tss & 0xffff);
+    gdt.tss_entry.base1 = (u8)((tss >> 16) & 0xff);
+    gdt.tss_entry.flags = 0x89;
+    gdt.tss_entry.flags1 = 0;
+    gdt.tss_entry.base2 = (u8)((tss >> 24) & 0xff);
+    gdt.tss_entry.base3 = (u32)(tss >> 32);
+    gdt.tss_entry.resv = 0;
 }
 
 void init_tss(u64 rsp0) {
-    char *ptr_c = (char *)&tss_entry;
-    for (usize i = 0; i < sizeof(tss_entry_t); i++) {
+    char *ptr_c = (char *)&daTSS;
+    for (usize i = 0; i < sizeof(tss); i++) {
         ptr_c[i] = (char)0;
     }
-    tss_entry.rsp0 = rsp0;
+    daTSS.rsp0 = rsp0;
 }
 
 int gdt_init(u64 *rsp0) {
     set_gdt_entry(0, 0, 0, 0, 0);             // NULL segment
     set_gdt_entry(1, 0, 0, 0b10011010, 0xA0); // Ring 0 code
-    set_gdt_entry(2, 0, 0, 0b10010010, 0xA0); // Ring 0 data
+    set_gdt_entry(2, 0, 0, 0b10010010, 0xC0); // Ring 0 data
     set_gdt_entry(3, 0, 0, 0b10111010, 0xA0); // Ring 1 code
-    set_gdt_entry(4, 0, 0, 0b10110010, 0xA0); // Ring 1 data
+    set_gdt_entry(4, 0, 0, 0b10110010, 0xC0); // Ring 1 data
     set_gdt_entry(5, 0, 0, 0b11011010, 0xA0); // Ring 2 code
-    set_gdt_entry(6, 0, 0, 0b11010010, 0xA0); // Ring 2 data
+    set_gdt_entry(6, 0, 0, 0b11010010, 0xC0); // Ring 2 data
     set_gdt_entry(7, 0, 0, 0b11111010, 0xA0); // Ring 3 code
-    set_gdt_entry(8, 0, 0, 0b11110010, 0xA0); // Ring 3 data
-    set_tss_gate(9, (u64)&tss_entry, sizeof(tss_entry_t));
+    set_gdt_entry(8, 0, 0, 0b11110010, 0xC0); // Ring 3 data
+    set_tss_gate((u64)&daTSS);
 
     gdtr.size = sizeof(gdt) - 1;
     gdtr.offset = (u64)&gdt;
@@ -57,6 +59,6 @@ int gdt_init(u64 *rsp0) {
     return 0;
 }
 
-void set_kernel_stack(u64 *stack) { tss_entry.rsp0 = (u64)stack; }
+void set_kernel_stack(u64 *stack) { daTSS.rsp0 = (u64)stack; }
 
 #endif
--- src/arch/x86_64/gdt.h
+++ src/arch/x86_64/gdt.h
@@ -26,7 +26,18 @@ typedef struct {
     u64 reserved2;
     u16 reserved3;
     u16 iomap_base;
-} __attribute__((packed)) tss_entry_t;
+} __attribute__((packed)) tss;
+
+typedef struct {
+  u16 length;
+  u16 base;
+  u8  base1;
+  u8  flags;
+  u8  flags1;
+  u8  base2;
+  u32 base3;
+  u32 resv;
+} __attribute__((packed)) tss_entry;
 
 typedef struct
 {
@@ -37,6 +48,11 @@ typedef struct
     u8 limit1_flags;
     u8 base2;
 } __attribute__((packed)) gdt_entry;
+
+typedef struct {
+  gdt_entry gdt_entries[9];
+  tss_entry tss_entry;
+} __attribute__((packed)) gdt_table;
 
 int gdt_init(u64* rsp0);
 void set_kernel_stack(u64* stack);
