82a4c6fccff42e3f25de0eeb4bca05a795328916
lolguy91 <retek1544@gmail.com>
2024-05-25 09:44:16 +0000

    added the skeleton for the event syscalls

--- src/core/sys/syscall.c
+++ src/core/sys/syscall.c
@@ -9,6 +9,18 @@ u64 syscall(u64 number, UNUSED u64 arg1, UNUSED u64 arg2, UNUSED u64 arg3) {
         // TODO: exit codes
         sched_kill(sched_current_pid);
         break;
+    case 1: // sys_event_create
+        break; // TODO
+    case 2: // sys_event_destroy
+        break; // TODO
+    case 3: // sys_event_subscribe
+        break; // TODO
+    case 4: // sys_event_unsubscribe
+        break; // TODO
+    case 5: // sys_event_fire
+        break; // TODO 
+    case 6: // sys_event_end
+        break; // TODO
     default:
         log_error("Syscall %d not implemented", number);
     }
