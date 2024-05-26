28373ccbfe3713a10e192298e6cc29ffcce0d83a
lolguy91 <retek1544@gmail.com>
2024-05-26 12:10:02 +0000

    bru

--- src/core/sys/syscall.c
+++ src/core/sys/syscall.c
@@ -12,22 +12,22 @@ u64 syscall(u64 number, u64 arg1, u64 arg2,UNUSED u64 arg3) {
         break;
     case 1: // sys_event_create
         return event_create();
-        break; // TODO
+        break;
     case 2: // sys_event_destroy
         event_destroy(arg1);
-        break; // TODO
+        break;
     case 3: // sys_event_subscribe
         event_subscribe(sched_current_pid, arg1, (void*)arg2);
-        break; // TODO
+        break;
     case 4: // sys_event_unsubscribe
         event_unsubscribe(sched_current_pid, arg1);
-        break; // TODO
+        break;
     case 5: // sys_event_fire
         event_fire(arg1, (void*)arg2);
-        break; // TODO 
+        break;
     case 6: // sys_event_end
-        return event_exit();
-        break; // TODO
+        event_exit();
+        break;
     default:
         log_error("Syscall %d not implemented", number);
     }
