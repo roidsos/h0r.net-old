d8c5916f815d304e339c66c025a8e8d638b8d44d
lolguy91 <retek1544@gmail.com>
2024-06-02 16:44:30 +0000

    removed test

--- src/core/sys/wakeup/gaia.c
+++ src/core/sys/wakeup/gaia.c
@@ -1,13 +1,8 @@
 #include <libk/stdbool.h>
 #include <vendor/printf.h>
-#include <core/mm/heap.h>
-#include "drivers/storage/AHCI.h"
 
 void gaia_main(void) {
     printf("TODO: Startup tasks\n");
-    char msgbuf[512];
-    ahci_use_device(&abar->ports[0], 0,(u8*)msgbuf,1,false);
-    printf("msgbuf: %s\n",msgbuf);
     while (true)
         ;
 }
