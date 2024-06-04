5a0127c4d9b67b013c8d2ed4bd2b9439357a496c
lolguy91 <retek1544@gmail.com>
2024-06-04 13:00:15 +0000

    made Gaia start the test process

--- src/core/sys/wakeup/gaia.c
+++ src/core/sys/wakeup/gaia.c
@@ -1,7 +1,9 @@
 #include <libk/stdbool.h>
 #include <vendor/printf.h>
+#include "core/sys/executive/ELF/elf.h"
 
 void gaia_main(void) {
     printf("TODO: Startup tasks\n");
+    exec_elf("bin/test", "test", false);
     while (true);
 }
